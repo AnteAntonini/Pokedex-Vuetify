79cf7e5bb3d7880f61a26e0c23feb08732a73826
AnteAntonini <anteantonini.mtk@hotmail.com>
2020-10-26 23:17:17 +0100

    dodao navigation drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,7 @@
 <template>
-    <v-app-bar
-      app
-      dense 
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <nav>
+    <v-app-bar app dense>     <!-- dene smanjuje onoliko koliko moze -->
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-switch
         v-model="$vuetify.theme.dark"
@@ -12,10 +10,16 @@
         label="Vuetify Theme Dark"
         persistent-hint
       ></v-switch>
-      
+
       <v-spacer></v-spacer>
 
-      <v-toolbar-title  class="text-center" align-center text-center justify-center>Application</v-toolbar-title>
+      <v-toolbar-title
+        class="text-center"
+        align-center
+        text-center
+        justify-center
+        >Application</v-toolbar-title
+      >
 
       <v-spacer></v-spacer>
 
@@ -23,20 +27,35 @@
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
-</template>
 
-<script>
-  export default {
-    //
-  }
-</script>
+    <v-navigation-drawer v-model="drawer" app temporary dark src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"> 
+      <v-list nav>            <!--  nav styling that reduces the width v-list-item takes up as well as adding a border radius. -->
+        <v-list-item v-for="([icon,text,route], i) in items" :key="i" router :to="route">
+          
+          <v-list-item-icon>
+            <v-icon>{{icon}}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{text}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>  
+    </v-navigation-drawer>   <!-- temporary znaci da navigation drawer ide preko, bez toga inace gurne stranicu -->
+  </nav>
+</template>
 
 <script>
 export default {
   name: "Navbar",
 
   data: () => ({
-    
+    drawer: false,
+    items: [
+        ['mdi-email', 'Inbox', '/'],
+        ['mdi-account-supervisor-circle', 'Supervisors', '#'],
+        ['mdi-clock-start', 'About', '/about'],
+      ]
   })
 };
 </script>
