8f53e21a2f33ea754f34d837f207278e2633c37c
AnteAntonini <anteantonini.mtk@hotmail.com>
2020-10-26 23:59:18 +0100

    smanjio v-switch

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,20 @@
 <template>
-  <nav>
-    <v-app-bar app dense>     <!-- dene smanjuje onoliko koliko moze -->
+  <nav id="navbar">
+    <v-app-bar app dense>     <!-- v-list can be lowered with dense property. -->
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-switch
         v-model="$vuetify.theme.dark"
-        hint="This toggles the global state of the Vuetify theme"
-        inset
+        hint="This toggles theme mode"
         label="Vuetify Theme Dark"
         persistent-hint
-      ></v-switch>
+        dense
+      ></v-switch>  <!-- inset = Enlarge the v-switch track to encompass the thumb -->
 
-      <v-spacer></v-spacer>
-
-      <v-toolbar-title
-        class="text-center"
-        align-center
-        text-center
-        justify-center
-        >Application</v-toolbar-title
-      >
+      
+      <v-toolbar-title class="flex text-center">    <!-- za centriranje title u sredinu toolbar-a !!-->
+        <span>Application</span>
+        </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
@@ -29,12 +24,12 @@
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" app temporary dark src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"> 
-      <v-list nav>            <!--  nav styling that reduces the width v-list-item takes up as well as adding a border radius. -->
+      <v-list shaped>            <!--  nav styling that reduces the width v-list-item takes up as well as adding a border radius. -->
         <v-list-item v-for="([icon,text,route], i) in items" :key="i" router :to="route">
           
-          <v-list-item-icon>
+          <v-list-item-action>
             <v-icon>{{icon}}</v-icon>
-          </v-list-item-icon>
+          </v-list-item-action>
 
           <v-list-item-content>
             <v-list-item-title>{{text}}</v-list-item-title>
@@ -59,3 +54,7 @@ export default {
   })
 };
 </script>
+
+<style>
+
+</style>
